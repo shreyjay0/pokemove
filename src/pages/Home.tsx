3c4ea160f6805f44105eb110544e304e5cad3d56
Shreyansh Jain <shreyjay0@gmail.com>
2022-04-09 17:41:08 +0530

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const onStartSession = () => {
+    window.location.href = "/session/catch";
+  };
+  return (
+    <div>
+      <div className="text-2xl">Welcome to pokemove</div>
+      <div className="p-8">
+        <div className="text-xl">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
+            onClick={onStartSession}
+          >
+            Start a session
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
